abd4a7e459fa6fe12b30fcacc8ffc71e82a65922
William Koller <wkoller25@gmail.com>
2020-06-30 22:02:54 -0300

    feat: add created controller

---
+++ server/modules/crush/controller.ts
@@ -0,0 +1,36 @@
+import Crush from './repository'
+
+class CrushController {
+	constructor() {}
+
+	getAll() {
+		return Crush.find({})
+	}
+
+	getById(id) {
+		return Crush.findById(id)
+	}
+
+	create(crush) {
+		return Crush.create(crush)
+	}
+
+	update(id, crush) {
+		const updateCrush = {
+			nome: crush.nome,
+			apelido: crush.apelido,
+			whatsapp: crush.whatsapp,
+			observacoes: crush.observacoes,
+			foto: crush.foto,
+			nota: crush.nota,
+		}
+
+		return Crush.findByIdAndUpdate(id, updateCrush)
+	}
+
+	delete(id) {
+		return Crush.remove(id)
+	}
+}
+
+export default new CrushController()
